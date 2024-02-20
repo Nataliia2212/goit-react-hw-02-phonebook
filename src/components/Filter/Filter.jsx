58fc0ed50752d42805895277b1a3358cc952e4b1
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-02-20 22:59:27 +0200

    fix

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import css from './Filter.module.css'
 
-export default function ({filter, onChange}) {
+export default function Filter ({filter, onChange}) {
   return (
     <div className={css.wrap}>
         <p>Find contacts by name</p>
